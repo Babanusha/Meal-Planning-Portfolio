586bb966a6ff73eda60fb8d9c74f79c5fd7f99a6
SebastianTK <83139977+SebastianKjone@users.noreply.github.com>
2024-12-02 22:33:54 +0100

    Frige methods + refactoring

--- src/main/module/Fridge.java
+++ src/main/module/Fridge.java
@@ -1,60 +1,145 @@
 package module;
 
+import static module.ApplicationSettings.STRING_KILOGRAMS;
+import static module.ApplicationSettings.STRING_LITERS;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+
 public class Fridge {
 
-  private ArrayList<Item> itemContainer;
+
+  private ArrayList<Item> listOfItems;
   private Item item;
 
 
   public Fridge() {
     init();
+    testInit();
   }
 
+  private void  testInit() { //TODO: Remove before completion
+    listOfItems.add(new Item("Milk", 1, STRING_LITERS, 20.03034, LocalDate.now().plusDays(2)));
+    listOfItems.add(new Item("Milk", 3, STRING_LITERS, 40.3402, LocalDate.now().plusDays(7)));
+    listOfItems.add(new Item("Bacon", 2, STRING_KILOGRAMS, 20.03032, LocalDate.now().plusDays(3)));
+    listOfItems.add(new Item("Kenguru", 1, STRING_LITERS, 30, LocalDate.now().plusDays(7)));
+
+
+  }
   private void init() {
-    itemContainer = new ArrayList<>();
+    listOfItems = new ArrayList<>();
   }
 
-
-
-
-  public void addItemToFridge(Item item) {
-    itemContainer.add(item);
-  }
-
-  public void createNewItem(String name, int amount, String unit) {
-    item = new Item(name, amount, unit);
+  public void createItemAndAddToFridge(String name, int quantity, String unit,
+      double productCost, LocalDate expirationDate) {
+    item = new Item(name, quantity, unit, productCost, expirationDate);
     addItemToFridge(item);
   }
 
-
-  private void removeItem(Item item) {
-    itemContainer.remove(item);
-  }
-  private ArrayList<Item> getItems() {
-    return itemContainer;
+  public Iterator<Item> iterateOverFridge() {
+    return listOfItems.iterator();
   }
 
-  public Iterator<Item> searchItems(String itemName) {
-      return itemContainer.stream()
-        .filter(item -> item.getName()
-            .equalsIgnoreCase(itemName)).iterator();
+  public void addItemToFridge(Item item) { //TODO: make private or remove?
+    listOfItems.add(item);
   }
 
 
-  public Iterator<Item> iterateFridge() {
-    return itemContainer.iterator();
+  public void removeItem(Item item) {
+    listOfItems.remove(item);
   }
 
-  public void testPrint() {         ////////// TEST
-    System.out.println("TEST");
-    for (Item item: itemContainer) {
-      System.out.println(item.getName() + " " + item.getQuantity() + " " + item.getUnit() + " " + item.getProductCost() + " " + item.getExpirationDate());
+
+  public Iterator<Item> searchForItem(String searchItem) {
+      return listOfItems.stream()
+        .filter(filteredListOfItems -> filteredListOfItems.getName()
+            .equalsIgnoreCase(searchItem)).iterator();
+  }
+
+
+
+  public int getSizeOfFridge() {
+    return listOfItems.size();
+  }
+  private ArrayList<Item> duplicateFridge() {
+    return new ArrayList<>(listOfItems);
+  }
+
+
+  private List<Item> fridgeSortedByExpirationDate() {
+    ArrayList<Item> arrayListToSort = duplicateFridge();
+    arrayListToSort.sort((firstItem, secondItem) -> firstItem.getExpirationDate()
+        .compareTo(secondItem.getExpirationDate()));
+    return arrayListToSort;
+  }
+
+  private List<Item> fridgeSortedByQuantity() {
+    ArrayList<Item> arrayListToSort = duplicateFridge();
+    arrayListToSort.sort((firstItem, secondItem) -> firstItem.getQuantity() - secondItem.getQuantity());
+    return arrayListToSort;
+  }
+
+  private List<Item> fridgeSortedByAlphabeticalOrder() { //TODO: change name of variable and method? ALL SORTS
+    ArrayList<Item> arrayListToSort = duplicateFridge();
+    arrayListToSort.sort((firstItem, secondItem) -> firstItem.getName().compareTo(secondItem.getName()));
+    return arrayListToSort;
+  }
+
+
+
+  public boolean editItemName(Item item, String newName) {
+    item.setName(newName);
+    return true;
+  }
+  public boolean editItemQuantity(Item item, int newQuantity) {
+    item.setQuantity(newQuantity);
+    return true;
+  }
+  public boolean editItemUnit(Item item, String newUnit) {
+    item.setUnit(newUnit);
+    return true;
+  }
+  public boolean editItemCost(Item item, double newCost) {
+    item.setProductCost(newCost);
+    return true;
+  }
+  public boolean editItemExpirationDate(Item item, LocalDate newDate) {
+    item.setExpirationDate(newDate);
+    return true;
+  }
+
+
+  public int specificItemInFridgeCount(String searchItem) {
+    int count = 0;
+    for (Item itemToCount : listOfItems) {
+      if (itemToCount.getName().equalsIgnoreCase(searchItem)) {
+        count++;
+      }
     }
+    return count;
   }
+
+
+  public Item retrieveNthOccurenceOfItem(int itemNumber, String searchItem) {
+    List<Item> listToSearch = listOfItems.stream().filter(itemInList -> itemInList.getName()
+        .equalsIgnoreCase(searchItem)).toList();
+    return listToSearch.get(itemNumber - 1);
+  }
+  public boolean isFridgeEmpty() {
+    return listOfItems.isEmpty();
+  }
+
+  public double calculateTotalCostOfItems() {
+    double totalcost = 0;
+    for (Item item : listOfItems) {
+      totalcost += item.getProductCost();
+    }return totalcost;
+  }
+
+
 }
 
 
