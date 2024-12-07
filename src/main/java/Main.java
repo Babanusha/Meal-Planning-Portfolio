be51026d5adef67cd674846cb3a9412e8a95dfa5
SebastianTK <83139977+SebastianKjone@users.noreply.github.com>
2024-12-07 20:38:21 +0100

    extracted controller methods, into three controller parts: main, fridge, & cookBook

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
 package redacted.path.;
 
 
-import redacted.path.controller.Controller;
+
+import redacted.path.controller.MainController;
 
 public class Main {
 
   public static void main(String[] args) {
-    new Controller();
+    new MainController();
   }
 }
